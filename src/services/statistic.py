f52396845ad474f3f3c82bdc0c5fa787a408ce37
EvgeniyBugaiov <evgeniy.bugaiov@gmail.com>
2023-10-25 21:25:38 +0200

    statistic service get_all_by_period can get date or datetime parameters

--- src/services/statistic.py
+++ src/services/statistic.py
@@ -1,7 +1,9 @@
 from functools import lru_cache
 
-from fastapi import Depends, HTTPException, status
-from sqlalchemy import select, and_, func
+from dateutil.parser import parse
+
+from fastapi import Depends
+from sqlalchemy import select, func
 from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -16,9 +18,9 @@ class StatisticService(BaseService):
     async def get_collections_by_date(self, *args) -> list[Statistic]:
         order_attribute, order_direction, date = args
         query = select(Statistic)\
-            .options(joinedload(Statistic.avtomat)\
-                .options(joinedload(Avtomat.route), joinedload(Avtomat.street)\
-                    .options(joinedload(Street.city))))\
+            .options(joinedload(Statistic.avtomat)
+                     .options(joinedload(Avtomat.route), joinedload(Avtomat.street)
+                              .options(joinedload(Street.city))))\
             .where(Statistic.event == 3)\
             .where(Statistic.time.like(f'%{date}%'))
         selected_data = (await self.db_session.execute(query)).scalars().all()
@@ -26,12 +28,16 @@ class StatisticService(BaseService):
         return ordered_data
 
     async def get_all_by_period(self, avtomat_number: int, start_period: str, end_period: str) -> list[Statistic]:
-        query = select(Statistic)\
-            .where(Statistic.avtomat_number == avtomat_number)\
-                .filter(and_(
-                    func.date(Statistic.time) >= start_period,
-                    func.date(Statistic.time) <= end_period
-                ))
+        query = select(Statistic).where(Statistic.avtomat_number == avtomat_number)
+        start_period = parse(start_period)
+        end_period = parse(end_period)
+        if start_period.hour == 0 and start_period.minute == 0 and end_period.hour == 0 and end_period.minute == 0:
+            query = query.filter(
+                func.date(Statistic.time) >= start_period,
+                func.date(Statistic.time) <= end_period
+            )
+        else:
+            query = query.filter(Statistic.time.between(start_period, end_period))
         selected_data = (await self.db_session.execute(query)).scalars().all()
         ordered_data = self.get_ordered_data(selected_data, 'time', 'desc')
         return ordered_data
